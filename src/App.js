8bddca548bde87d8d2e52caeb7da2aee9403d7de
Arsen <arsen.avetisyan.2000@mail.ru>
2020-09-30 22:47:00 +0300

    added Route, navlink ...

--- src/App.js
+++ src/App.js
@@ -5,17 +5,20 @@ import './App.css';
 import { SideBar } from './Component/SideBar/SideBar';
 import Header from './Component/Header/Header';
 import Profile from './Component/Content/Profile';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 const App = (props) => {
   return (
-    <div className="appWrapper">
-      <Header />
-      <SideBar />
-      <div class = "content">
-        <Profile m_mydata = {props.Iam}/>
-      </div>
+    <BrowserRouter>
+      <div className="appWrapper">
+        <Header />
+        <SideBar />
+        <div class = "content">
+          <Route path = "/Content" render = { () => <Profile m_mydata = {props.Iam}/> } />
+        </div>
   
-    </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/Component/SideBar/SideBar.jsx
+++ src/Component/SideBar/SideBar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom';
 import stl from './SideBar.module.css';
 
 
@@ -6,11 +7,11 @@ export const SideBar = () => {
     return (
         <div className = {stl.sideBarWrapper}>
             <nav>
-                <div className={stl.itemSidebar}><a href="#s">Профиль</a></div>
-                <div className={stl.itemSidebar}><a href="#s">Сообщения</a></div>
-                <div className={stl.itemSidebar}><a href="#s">Новости</a></div>
-                <div className={stl.itemSidebar}><a href="#s">Музыка</a></div>
-                <div className={stl.itemSidebar}><a href="#s">Настройки</a></div>
+                <div className={stl.itemSidebar}><NavLink to="/Content">Профиль</NavLink></div>
+                <div className={stl.itemSidebar}><NavLink to="#s">Сообщения</NavLink></div>
+                <div className={stl.itemSidebar}><NavLink to="#s">Новости</NavLink></div>
+                <div className={stl.itemSidebar}><NavLink to="#s">Музыка</NavLink></div>
+                <div className={stl.itemSidebar}><NavLink to="#s">Настройки</NavLink></div>
             </nav>
         </div>
     )
